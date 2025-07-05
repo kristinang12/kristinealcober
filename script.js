587841945993ffdaf6c107f7457633b3ece3a33d
kristinang12 <kristinangalcober12@gmail.com>
2025-07-05 16:25:39 +0800

    3rd commit

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById("joinBtn").addEventListener("click", function() {
     alert("Hello! You clicked Join Now.");
 });
 
-document.getElementById("form-contact").addEventListener("submit", function() {
-    setTimeout(function() {
-        window.location.href = "index.html";
-    }, 1000); // wait 1 second
-});
+
+// script.js
+function greet() {
+  alert("Welcome to my site!");
+}
--- wifi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ssid = shell_exec('netsh wlan show interfaces');
-preg_match('/^\s*SSID\s*:\s*(.+)$/m', $ssid, $matches);
-if (isset($matches[1])) {
-    echo "Wi-Fi Name (SSID): " . trim($matches[1]);
-} else {
-    echo "SSID not found.";
-}
-?>
