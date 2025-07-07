31ae1801f160aec51cd4a873798d8a060eca2ebe
kristinang12 <kristinangalcober12@gmail.com>
2025-07-07 19:10:38 +0800

    edit nAVBAR

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ document.getElementById("joinBtn").addEventListener("click", function() {
     alert("Hello! You clicked Join Now.");
 });
 
-
-// script.js
-function greet() {
-  alert("Welcome to my site!");
-}
