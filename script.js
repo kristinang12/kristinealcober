7aa2629ce9e522f48de24f840cd73cea344fd342
kristinang12 <kristinangalcober12@gmail.com>
2025-07-08 10:35:36 +0800

    edit gd

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ document.getElementById("joinBtn").addEventListener("click", function() {
     alert("Hello! You clicked Join Now.");
 });
 
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const previewImg = document.getElementById("preview-img");
+const closeBtn = document.getElementById("close");
+
+previewImg.onclick = () => {
+  modal.style.display = "block";
+  modalImg.src = previewImg.src;
+};
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
